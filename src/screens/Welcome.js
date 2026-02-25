ff01161c48a1c17ea310bf04853d9c513bb968d8
hacibahcee <hacibahcee@gmail.com>
2026-02-26 00:27:34 +0300

    edit logo

--- src/screens/Welcome.js
+++ src/screens/Welcome.js
@@ -9,7 +9,8 @@ const Welcome = () => {
   return (
       <SafeAreaView style={{ flex: 1, paddingVertical: 10, paddingHorizontal: 20 }}>
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-          <Image source={images.petevent_logo} style={{ width: 200, height: 200 }} />
+          <Image source={images.petevent_logo} style={{ width: 500, height: 500}} />
+          <Text style={{ fontSize: 24, fontWeight: 'bold', marginTop: 20, color: '#933CAC' }}>Hoşgelmişsen to Petevent!</Text>
         </View>
       </SafeAreaView>
   )
