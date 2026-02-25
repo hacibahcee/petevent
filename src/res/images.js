d73beb858c04397fce49521877a54e3440ae888f
hacibahcee <hacibahcee@gmail.com>
2026-02-25 23:31:28 +0300

    logo

---
+++ src/res/images.js
@@ -0,0 +1,5 @@
+const images = {
+    petevent_logo: require('./images/petevent_logo.png')
+}
+
+export default images;
--- src/screens/Welcome.js
+++ src/screens/Welcome.js
@@ -1,18 +1,15 @@
 import { useNavigation } from '@react-navigation/native'
 import { useEffect } from 'react'
-import { SafeAreaView,Text, View} from 'react-native'
+import { Image, Text, View} from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import images from '../res/images'
 
 const Welcome = () => {
-  const navigation = useNavigation()
-
-  useEffect(()=>{
-    setTimeout(() => navigation.reset({ routes: [{ name: 'Home' }] }), 2000);
-  },[])
-
+  
   return (
       <SafeAreaView style={{ flex: 1, paddingVertical: 10, paddingHorizontal: 20 }}>
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text style={{ marginVertical: 20, color:"#000000", fontSize: 16 }}>Welcome Screen</Text>
+          <Image source={images.petevent_logo} style={{ width: 200, height: 200 }} />
         </View>
       </SafeAreaView>
   )
