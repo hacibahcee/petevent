aeb6ac7312ed6f9f7266473d55273178208e9561
hacibahcee <hacibahcee@gmail.com>
2026-03-01 16:04:24 +0300

    iconAdd

--- src/screens/Welcome.js
+++ src/screens/Welcome.js
@@ -3,6 +3,8 @@ import { useEffect } from 'react'
 import { Image, Text, View} from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import images from '../res/images'
+import * as Animatable from 'react-native-animatable';
+import Icon from 'react-native-vector-icons/FontAwesome6';
 
 const Welcome = () => {
   
@@ -10,7 +12,10 @@ const Welcome = () => {
       <SafeAreaView style={{ flex: 1, paddingVertical: 10, paddingHorizontal: 20 }}>
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
           <Image source={images.petevent_logo} style={{ width: 500, height: 500}} />
-          <Text style={{ fontSize: 24, fontWeight: 'bold', marginTop: 20, color: '#933CAC' }}>Hoşgelmişsen to Petevent!</Text>
+          <Animatable.View duration={3000} delay={1500} animation={'fadeOut'}>
+            <Animatable.Text duration={3000} animation={'fadeIn'} style={{ marginVertical: 20, color:"#000000", fontSize: 16 }}>Hoşgelmişsen to Petevent!</Animatable.Text>
+            <Icon name="arrow-left" size={26} color="#000000" />
+          </Animatable.View>
         </View>
       </SafeAreaView>
   )
